e01b0356015ba18cc4b5a139cd3d34534c5b8a56
Pedro_moraes2000 <112527031+pedrodev2000@users.noreply.github.com>
2026-05-15 11:48:15 -0300

    subindo

---
+++ Dialogflow/funcoes.js
@@ -0,0 +1,51 @@
+import Consorcio from "../models/consorcio.js";
+
+export function criarMessengerCard(){
+    return {
+        type: "info",
+        title:"",
+        subtitle:"",
+        image: {
+            src : {
+                rawUrl:""
+            }
+        },
+        actionLink:""
+    }
+}
+
+
+export function criarCustomCard(){
+    return{
+        card: {
+            title:"",
+            subtitle:"",
+            imageUri:"",
+            buttons: [
+                {
+                    text:"botão",
+                    postback:""
+                }
+            ]
+        }
+    }
+}
+
+export default async function obterCardsConsorcios(tipoCard="custom"){
+
+    const cards = [];
+    const consorcio = new Consorcio();
+    const listaConsorcios = await consorcio.consultar();
+    for(const consorcio of listaConsorcios){
+        if(tipoCard == "custom"){
+            const cardCustom = criarCustomCard();
+            cardCustom.card.title = consorcio.codigo;
+            cardCustom.card.subtitle = consorcio.veiculo
+            cardCustom.card.imageUri = consorcio.imagem;
+            cards.push(cardCustom);
+        }
+        else{
+
+        }
+    }
+}
